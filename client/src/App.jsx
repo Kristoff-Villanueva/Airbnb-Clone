2a5193fe0ce8b3e8dc55fb09ec73ae3ccbf2d90a
Kristoff-Villanueva <kristoffer.villanueva18@gmail.com>
2023-02-19 23:32:23 +0800

    add register and login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,17 @@ import { Route, Routes } from "react-router";
 import LoginPage from "./pages/LoginPage";
 import "./App.css";
 import IndexPage from "./pages/IndexPage";
+import Layout from "./Layout";
+import Register from "./pages/RegisterPage";
 
 function App() {
 	return (
 		<Routes>
-			<Route index element={<IndexPage />} />
-			<Route path="/login" element={<LoginPage />} />
+			<Route path="/" element={<Layout />}>
+				<Route index element={<IndexPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/register" element={<Register />} />
+			</Route>
 		</Routes>
 	);
 }
